75e9715d43712fe2c2ddcebc6a86ea6f2d4872d0
Vansh Rana <vanshrana98050@gmail.com>
2026-07-30 21:23:20 +0530

    Add files via upload

---
+++ check_perfect_number.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    int n, i, sum = 0;
+    printf("Enter number: ");
+    scanf("%d", &n);
+
+    for(i = 1; i <= n/2; i++) {
+        if(n % i == 0)
+            sum += i;
+    }
+
+    if(sum == n)
+        printf("Perfect number");
+    else
+        printf("Not perfect");
+
+    return 0;
+}
