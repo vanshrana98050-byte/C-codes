c40ecd3b9a43224d4e772e4d00d4a248e3f15518
Vansh Rana <vanshrana98050@gmail.com>
2026-07-31 23:05:03 +0530

    Add files via upload

---
+++ factorial_using_a_function.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int fact(int n) {
+    int i, f = 1;
+    for(i = 1; i <= n; i++)
+        f *= i;
+    return f;
+}
+int main() {
+    int n;
+    scanf("%d", &n);
+    printf("Factorial = %d", fact(n));
+    return 0;
+}
