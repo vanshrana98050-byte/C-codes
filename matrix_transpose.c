5663c948e05a546902b10c290d89ed307212bfca
Vansh Rana <vanshrana98050@gmail.com>
2026-08-01 23:40:58 +0530

    Add files via upload

---
+++ matrix_transpose.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    int a[3][3], i, j;
+
+    printf("Enter matrix:\n");
+    for(i = 0; i < 3; i++)
+        for(j = 0; j < 3; j++)
+            scanf("%d", &a[i][j]);
+
+    printf("Transpose:\n");
+    for(i = 0; i < 3; i++) {
+        for(j = 0; j < 3; j++)
+            printf("%d ", a[j][i]);
+        printf("\n");
+    }
+
+    return 0;
+}
