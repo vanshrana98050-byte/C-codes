51e26f89c6ee8cc49df30da6a5cf535264d03917
Vansh Rana <vanshrana98050@gmail.com>
2026-08-04 22:30:25 +0530

    Add files via upload

---
+++ prime_using_fuction.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int prime(int n) {
+    int i;
+    for(i = 2; i <= n/2; i++)
+        if(n % i == 0)
+            return 0;
+    return 1;
+}
+int main() {
+    int n;
+    scanf("%d", &n);
+    if(prime(n) && n > 1)
+        printf("Prime");
+    else
+        printf("Not Prime");
+    return 0;
+}
